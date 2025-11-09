e1d8c5d5515772bd38c1be2944bc4955999b07a1
AbhinavGupta6--5 <motilalg25@gmail.com>
2025-11-09 10:46:15 +0530

    Add Cloudinary for pdf & Img

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import  {User} from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/dataUri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
   try {
@@ -12,6 +14,9 @@ export const register = async (req, res) => {
         success: false,
       });
     }
+    const file = req.file;
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
     const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({
@@ -28,6 +33,9 @@ export const register = async (req, res) => {
       phoneNumber,
       password: hashedPassword,
       role,
+      profile:{
+        profilePhoto: cloudResponse.secure_url
+      }
     });
 
     return res.status(201).json({
@@ -120,10 +128,17 @@ export const logout = async (req, res) => {
 export const updateProfile = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, bio, skills } = req.body;
+    // console.log(fullname, email, phoneNumber, bio, skills);
     const file = req.file;
+
+    
+    // cloudinary ayega idhar
+    const fileUri = getDataUri(file);
+
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
     
 
-    // cloudinary ayega idhar
+
 
     let skillsArray;
     if(skills){
@@ -148,6 +163,10 @@ export const updateProfile = async (req, res) => {
     if(skillsArray) user.profile.skills = skillsArray;
 
     // resume comes later here...
+    if(cloudResponse){
+      user.profile.resume = cloudResponse.secure_url   // save the cloudinary url
+      user.profile.resumeOriginalName = file.originalname    // save the original file name
+    }
     await user.save();
 
     user = {
